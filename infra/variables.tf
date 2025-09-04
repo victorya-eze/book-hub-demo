82c04b47d0c629d03bba426f6e1b9b4d8467ae1d
Victory Eze <161460976+victorya-eze@users.noreply.github.com>
2025-09-04 18:48:01 +0100

    fixed terrafrom issues

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,31 @@
-variable "app_name"        { type = string  default = "eb-nextjs-demo" }
-variable "env_name"        { type = string  default = "dev" }
-variable "region"          { type = string  default = "eu-west-2" }
-variable "version_label"   { type = string  default = "v-0" }
-variable "source_bundle"   { type = string  default = "../app.zip" }
+# infra/variables.tf
+
+variable "app_name" {
+  type    = string
+  default = "eb-nextjs-demo"
+}
+
+variable "env_name" {
+  type    = string
+  default = "dev"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-2" # change if needed
+}
+
+variable "version_label" {
+  type    = string
+  default = "v-0" # CI overrides this per run
+}
+
+variable "source_bundle" {
+  type    = string
+  default = "../app.zip"
+}
+
+# Pick a current EB Docker platform ARN for YOUR region (store in secret if you like)
 variable "platform_arn" {
   type    = string
   default = "arn:aws:elasticbeanstalk:eu-west-2::platform/Docker running on 64bit Amazon Linux 2/5.10.0"
